2333f945bb34ef03d1ba8b7b0c3dc6cd50ecc06a
cog-priyanshu <2406638@cognizant.com>
2025-11-23 13:22:13 +0530

    Minor bug fix

--- SearchService/Consumer/AuctionUpdateConsumer.cs
+++ SearchService/Consumer/AuctionUpdateConsumer.cs
@@ -1,6 +1,41 @@
-﻿namespace SearchService.Consumer
+﻿using AutoMapper;
+using Contracts;
+using MassTransit;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Consumer
 {
-    public class AuctionUpdateConsumer
+    public class AuctionUpdateConsumer : IConsumer<AuctionUpdated>
     {
+        private readonly IMapper _mapper;
+
+        public AuctionUpdateConsumer(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+        
+        public async Task Consume(ConsumeContext<AuctionUpdated> context)
+        {
+            Console.WriteLine("----> Consuming Update Message");
+            var item = _mapper.Map<Item>(context.Message);
+            
+            var result = await DB.Update<Item>()
+                .Match(a => a.ID == context.Message.Id)
+                .ModifyOnly(x => new
+                {
+                    x.Make,
+                    x.Model,
+                    x.Mileage,
+                    x.Color,
+                    x.Year
+                }, item)
+                .ExecuteAsync();
+                
+            if (!result.IsAcknowledged)
+            {
+                throw new MessageException(typeof(AuctionUpdated), "Problem updating MongoDB");
+            }
+        }
     }
 }
